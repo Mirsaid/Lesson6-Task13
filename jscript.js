4b1d4d657852011fceac263749756143da840ae5
Mirsaid Alizadeh <mirsaid.alizadeh@pasha-technology.com>
2021-08-10 10:04:37 +0400

    last changes by me

--- jscript.js
+++ jscript.js
@@ -1,13 +1,25 @@
-let animals = ['parrot','cat','dog','hare','lion']
+let bands = ['acdc','pink floyd','red hot chili peppers','scorpions','queen','nirvana']
 
-let askFromUser = prompt("What is your favorite animal?")
+let askFromUser = prompt("What is your favorite band?")
 // console.log(askFromUser.toLowerCase())
 
 // console.log(animals.indexOf(askFromUser))
 
-if(animals.indexOf(askFromUser.toLowerCase())>0){
-    alert("YEAH!")
+if(bands.indexOf(askFromUser.toLowerCase())>=0){
+    alert("YEAH I LOVE THEM!")
 }else{
-    alert("NAH")
+    alert("Nah. They're pretty lame.")
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
